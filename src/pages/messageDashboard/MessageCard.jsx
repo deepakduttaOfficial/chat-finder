17e5badb733b3b7ec9e75f7ad81b914579bf836b
Deepak Dutta <coder.deepakyt@gmail.com>
2023-02-11 20:49:24 +0530

    Link detect function implemented with regex

--- src/pages/messageDashboard/MessageCard.jsx
+++ src/pages/messageDashboard/MessageCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box, Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Link,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 //  Custom style
 import { containerStyle, textContainerStyle, timeStyle } from "./style";
 // import my npm package date time formatter
@@ -7,6 +14,12 @@ import datetimeFormatter from "datetime-formatter-js";
 
 const MessageCard = ({ mess, currentUser }) => {
   // console.log(new Date().toDateString());
+  const isURL = (string) => {
+    var res = string?.match(
+      /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+    );
+    return res !== null;
+  };
 
   return (
     <Flex
@@ -23,10 +36,22 @@ const MessageCard = ({ mess, currentUser }) => {
         {...textContainerStyle}
       >
         {mess?.photoURL && <Image src={mess.photoURL} w="52" />}
+        {isURL(mess.message) ? (
+          <Link
+            color={"blue.400"}
+            fontSize="sm"
+            href={mess.message}
+            target="_blank"
+            mr="12"
+          >
+            {mess.message}
+          </Link>
+        ) : (
+          <Text mr="12" color={useColorModeValue("gray.700", "white")}>
+            {mess.message}
+          </Text>
+        )}
 
-        <Text mr="12" color={useColorModeValue("gray.700", "white")}>
-          {mess.message}
-        </Text>
         <Text
           aria-level="time"
           color={useColorModeValue("gray.500", "#ffffff99")}
